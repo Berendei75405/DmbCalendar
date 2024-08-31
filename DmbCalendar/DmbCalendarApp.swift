39e72759f859374d982cf52e2c9c980906446e7d
Novgorodtcev <73novgor@mail.ru>
2024-08-31 21:02:26 +0700

    Initial Commit

---
+++ DmbCalendar/DmbCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DmbCalendarApp.swift
+//  DmbCalendar
+//
+//  Created by Novgorodcev on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DmbCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DmbCalendar/MainModule/CalendarView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DmbCalendar
+//
+//  Created by Novgorodcev on 31.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
